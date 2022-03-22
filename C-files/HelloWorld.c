3f5445e6fc3143b1a6e1e3dbe7d32da0760a4bc0
muhabeid <muhabeid@gmail.com>
2022-03-22 17:48:08 +0300

    First program

---
+++ C-files/HelloWorld.c
@@ -0,0 +1,13 @@
+// #include - preprocessor commands that tells the compiler to include content of a file
+// std - standard
+// io - input/output
+#include <stdio.h>
+
+int main()
+{
+  printf("I like Pizza\n");
+  printf("It's really good\n");
+  
+// return 0 if the program runs successful without errors, and returns 1 if there's an error.
+  return 0;
+}
