6cd97e55f227eede1fda3b4172b6bbd7a6a2d448
muhabeid <muhabeid@gmail.com>
2022-03-22 18:29:50 +0300

    Comments

---
+++ C-files/comments.c
@@ -0,0 +1,16 @@
+// Comments is a text that is ignored by the compiler that is used as an explanation, decription or a note for yourself or any reading the code.
+
+#include <stdio.h>
+int main(){
+
+  // This is a single line comment
+  /*
+      This
+      is 
+      a
+      multiline
+      comments
+  */
+  printf("I like pizza\n");
+  return 0;
+}
