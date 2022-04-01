1f437476133dcd88870033ad3eb551504bb0279e
muhabeid <muhabeid@gmail.com>
2022-04-01 23:08:13 +0300

    If statements

--- C-files/ifstatements.c
+++ C-files/ifstatements.c
@@ -1,10 +1,30 @@
 /**
  * @file ifstatements.c
  * @author muhabeid
- * @brief 
+ * @brief - if statements are used to add some choices to a program 
  * @version 0.1
  * @date 2022-03-24
  * 
  * @copyright Copyright (c) 2022
- * 
  */
+#include <stdio.h>
+
+int main()
+{
+  int age;
+
+  printf("\nEnter your age: ");
+  scanf("%d", age);
+
+  if(age >= 18){
+    printf("You are now signed up!");
+  }
+  else if(age < 0){
+    printf("You haven't been born yet!");
+  }
+  else{
+    printf("You are too young to sign up!");
+  }
+
+  return 0;
+}
