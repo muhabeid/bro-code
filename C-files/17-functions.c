cb5a70b646c84e1f8318a35ca8574b4746b2a2fb
muhabeid <muhabeid@gmail.com>
2022-04-02 16:20:54 +0300

    Will update this later

---
+++ C-files/17-functions.c
@@ -0,0 +1,19 @@
+/**
+ * @file 17-functions.c
+ * @author muhabeid
+ * @brief - Functions is a small section of code that is executed whenever we call it.
+ * @version 0.1
+ * @date 2022-04-02
+ * 
+ * @copyright Copyright (c) 2022
+ */
+#include <stdio.h>
+
+int main()
+{
+
+  // 
+
+
+  return 0;
+}
